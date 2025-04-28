69ef5944b02efe7062720b5fc05ce696862b0d91
jammuhia <santajaggered1@gmail.com>
2025-04-28 12:44:21 -0500

    Update HotelSystem.java

--- src/main/java/com/mycompany/hotelsystem/HotelSystem.java
+++ src/main/java/com/mycompany/hotelsystem/HotelSystem.java
@@ -3,12 +3,15 @@ package com.mycompany.hotelsystem;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.Scanner;
+import java.util.*;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class HotelSystem {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
 
         // Initialize system components
         GuestManager guestManager = new GuestManager();
@@ -20,12 +23,95 @@ public class HotelSystem {
         // Use MM-dd-yyyy for date input
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
 
-        System.out.println("Welcome to the Hotel Reservation and Valet System!");
+        
+        //~~~System.out.println("Welcome to the Hotel Reservation and Valet System!");
         boolean running = true;
 
-        while (running) {
+        //while (running) {
             // Main menu
-            System.out.println("\nMain Menu:");
+            JFrame hotelFrame = new JFrame("Hotel Page");
+            hotelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JLabel menuLabel = new JLabel("Main Menu");
+            JButton registerButton = new JButton("Register Guest");
+            JButton reservationButton = new JButton("Make a Reservation");
+            JButton valetReqButton = new JButton("Request Valet");
+            JButton checkoutButton = new JButton("Check-out Guest");
+            JButton adminRepButton = new JButton("Generate Admin Report");
+            JButton exitButton = new JButton("Exit");
+
+            JPanel menuPanel = new JPanel();
+            menuPanel.add(menuLabel);
+            menuPanel.add(registerButton);
+            menuPanel.add(reservationButton);
+            menuPanel.add(valetReqButton);
+            menuPanel.add(checkoutButton);
+            menuPanel.add(adminRepButton);
+            menuPanel.add(exitButton);
+
+            hotelFrame.getContentPane().add(menuPanel, BorderLayout.CENTER);
+            hotelFrame.pack();
+            hotelFrame.setResizable(false);
+            hotelFrame.setVisible(true);
+
+            registerButton.addActionListener(e -> {
+                String guestName = JOptionPane.showInputDialog("Enter the guest name");
+                Guest guest = new Guest();
+                guest.setName(guestName);
+                guest.setGuestId((int) (Math.random() * 10000));
+                guestManager.addGuest(guest);
+
+                JOptionPane.showMessageDialog(hotelFrame, "Guest registered with ID: " + guest.getGuestId());
+            });
+
+            reservationButton.addActionListener(f -> {
+                try {
+                    int guestID = Integer.parseInt(JOptionPane.showInputDialog("Enter guest ID"));
+
+                    int roomNumber = Integer.parseInt(JOptionPane.showInputDialog("Enter room number"));
+
+                    LocalDate checkIn = LocalDate.parse(JOptionPane.showInputDialog("Enter check-out date (MM-DD-YYYY)"), formatter);
+
+                    LocalDate checkOut = LocalDate.parse(JOptionPane.showInputDialog("Enter check-out date (MM-DD-YYYY)"), formatter);
+
+                    if (!checkOut.isAfter(checkIn)) {
+                        JOptionPane.showMessageDialog(hotelFrame, "Error : Check-out date must be after Check-in date");
+                    }
+
+                    boolean available = reservationSystem.checkRoomAvailability(roomNumber, checkIn, checkOut);
+                    if (!available) {
+                        JOptionPane.showMessageDialog(hotelFrame, "Sorry, room is not available for those dates.");
+                    } else {
+                        reservationSystem.createReservation(guestID, roomNumber, checkIn, checkOut);
+                        JOptionPane.showMessageDialog(hotelFrame, "Reservation successfully created.");
+                    }
+
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(hotelFrame, "Invalid number input. Please enter numbers only.");
+                } catch (DateTimeParseException e) {
+                    JOptionPane.showMessageDialog(hotelFrame, "Invalid date format. Use MM-dd-yyyy.");
+                }
+            });
+
+            valetReqButton.addActionListener(g -> {
+                guestManager.valetRequest();
+            });
+
+            checkoutButton.addActionListener(h -> {
+                checkOutSystem.processCheckOut();
+            });
+
+            adminRepButton.addActionListener(i -> {
+                adminManager.generateGuestValetReport();
+            });
+
+            exitButton.addActionListener(j -> {
+                JOptionPane.showMessageDialog(hotelFrame, "Goodbye!");
+                //running = false;
+                hotelFrame.dispose();
+            });
+
+            /*~~~System.out.println("\nMain Menu:");
             System.out.println("1. Register Guest");
             System.out.println("2. Make a Reservation");
             System.out.println("3. Request Valet");
@@ -34,10 +120,10 @@ public class HotelSystem {
             System.out.println("6. Exit");
 
             System.out.print("Enter choice: ");
-            String input = scanner.nextLine();
+            String input = scanner.nextLine();*/
 
-            switch (input) {
-                case "1":
+            //switch () {
+                /*case "1":
                     // Guest registration
                     System.out.print("Enter guest name: ");
                     String name = scanner.nextLine();
@@ -48,9 +134,9 @@ public class HotelSystem {
                     guestManager.addGuest(guest);
 
                     System.out.println("Guest registered with ID: " + guest.getGuestId());
-                    break;
+                    break;*/
 
-                case "2":
+                /*~~~case "2":
                     // Make a reservation
                     try {
                         System.out.print("Enter guest ID: ");
@@ -83,9 +169,9 @@ public class HotelSystem {
                     } catch (DateTimeParseException e) {
                         System.out.println("Invalid date format. Use MM-dd-yyyy.");
                     }
-                    break;
+                    break;*/
 
-                case "3":
+                /*case "3":
                     // Request Valet
                     guestManager.valetRequest();
                     break;
@@ -108,9 +194,9 @@ public class HotelSystem {
 
                 default:
                     System.out.println("Invalid option. Try again.");
-            }
-        }
+            }*/
+        //}
 
-        scanner.close();
+        //scanner.close();
     }
 }
